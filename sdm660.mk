d20327764672aa5fe5321db8256a82bbef5e1288
Sonal Singh <sonal.singh.19993@gmail.com>
2019-06-10 12:38:48 +0000

    [SQUASH] sdm660-common: Import ZenParts v2.0
    Features:-
    • Enable/Disable Camera2 API Toggle
    • Torch Brightness Control
    • Vibration Control
    • Display Color Calibration
    • Grayscale & KCAL Presets
    • Spectrum Profiles
    • Dirac Sound Enhancer
    • Headphone Gain
    • Microphone Gain
    • Added Spectrum Support
    
    Refrenced from:- https://github.com/xiaomi-sdm660/android_device_xiaomi_sdm660-common.git
    
    Change-Id: I711946a6e954e19ed66e4335fc96da3b986fc940
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Tiktodz <kuplemarkeple@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -455,3 +455,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+    
+# ZenParts
+PRODUCT_PACKAGES += \
+    init.spectrum.rc \
+    init.spectrum.sh \
+    init.zenparts.rc \
+    init.zenparts.sh \
+    ZenParts
