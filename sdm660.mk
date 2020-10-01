a4373a8cf81494116b61026ad67b8daf84868efc
Lunarixus <Nathanb644@hotmail.co.uk>
2020-10-01 16:41:11 +0000

    sdm660-common: Remove packages at build time  - Update it to align it to https://gitlab.com/EvoX/vendor_gapps
    Signed-off-by: Tiktodz <kuplemarkeple@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -355,6 +355,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePkgs
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.btmac.sh \
