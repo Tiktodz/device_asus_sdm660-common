54e78bc30592ff95664c6e72890f02be079ee09c
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-02-12 18:27:02 +0530

    [SQUASH] sdm660-common: ZenParts: sepolicy: Add sepolicies
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    sdm660-common: ZenParts: sepolicy: Add KCAL sepolicy
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    sdm660-common: ZenParts: sepolicy: Add vibrator sepolicy
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    sdm660-common: ZenParts: sepolicy: Add spectrum sepolicy
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    sdm660-common: ZenParts: sepolicy: Add camera2 API toggle sepolicy
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    sdm660-common: ZenParts: sepolicy: Add torch's brightness control sepolicy
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    sdm660-common: ZenParts: sepolicy: Add dirac sepolicy
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    sdm660-common: ZenParts: sepolicy: Add headphone & microphone gain sepolicy
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    
    [TMP] sdm660-common: Allow ignoring neverallows
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Tiktodz <kuplemarkeple@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -188,6 +188,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # SurfaceFlinger
 TARGET_USE_AOSP_SURFACEFLINGER := true
