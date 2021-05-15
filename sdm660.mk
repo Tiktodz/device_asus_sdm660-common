340789dda4c5019457e4564e8c85dcccf62627d0
Erfan Abdi <erfangplus@gmail.com>
2021-05-16 03:41:40 +0600

    sdm660-common: Don't enforce RRO for all overlays * Breaking /product/overlay * Also this makes many platform specific overlays with current system API on vendor partition which breaks newer android version compatibilities (broken stuffs in gsis)
    Change-Id: I45eb18b0754726fb4f779521a0245dfcb1259b17

--- sdm660.mk
+++ sdm660.mk
@@ -270,8 +270,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-nad
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
