3cc7739b1cb533b0fe445303abce592abad65e92
RONIN808 <shubhamshedage808@gmail.com>
2021-10-25 06:20:14 +0000

    sdm660-common: Removed few overlays for now

--- sdm660.mk
+++ sdm660.mk
@@ -284,9 +284,6 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Permissions
 PRODUCT_COPY_FILES += \
