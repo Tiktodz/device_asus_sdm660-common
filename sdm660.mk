0e822406727f074a78d94ca8036d526bc44b3a54
Tiktodz <kuplemarkeple@gmail.com>
2021-11-20 23:00:42 +0000

    sdm660-common: overlay: lineage => fluid

--- sdm660.mk
+++ sdm660.mk
@@ -264,7 +264,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-fluid
 
 # Permissions
 PRODUCT_COPY_FILES += \
