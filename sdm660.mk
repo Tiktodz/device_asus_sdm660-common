9f9a3b066912744a4496e497becfcfbb8b19583e
Tiktodz <kuplemarkeple@gmail.com>
2022-01-18 06:23:03 +0000

    sdm660-common: Removed few overlays
    Signed-off-by: Tiktodz <kuplemarkeple@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -273,8 +273,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
